bff073c2745497589acc4643ecb0ebf8f7ec79c1
luisEfe80 <luismendez80@hotmail.com>
2021-07-28 21:09:53 -0400

    Ajuste de contenido

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -6,6 +6,28 @@
       .titulo-principal__numero
         span  1
       h1 Introducción aplicaciones móviles híbridas
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5.mb-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/t1-i1.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-4.shadow-box
+            p.mb-0 Existen varios tipos de aplicaciones móviles, que se pueden clasificar según la manera en la que se utilizan e integran con el sistema operativo: nativas, web e híbridas. <b>A continuación, se exponen sus principales características, algunas de las cuales se evidencian en la siguiente figura.</b>
+
+    .row.justify-content-center.mb-5
+      .col-lg-8   
+
+
+
+
+
+
+
+
+
+
+
 
     //- El componente "Muestras" tiene las aplicaciones de todos los componentes de la base
     //- se encuentra en src/components/Muestras.vue
