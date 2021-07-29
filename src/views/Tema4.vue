f720dbc10a76ed670557744b58b4a7e3e07eceec
luisEfe80 <luismendez80@hotmail.com>
2021-07-29 01:57:19 -0400

    Adicion de contenido

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -19,13 +19,86 @@
 
     .row
       .col-lg-3
+        //-- Aqui va el overlay card
       .col-lg-3
       .col-lg-3
       .col-lg-3
 
 
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta--gris")
+          .row.justify-content-center.align-items-center(titulo="React Native")       
+            .col-md-2
+              figure
+                img(src='@/assets/curso/t4-ic1.svg', alt='Texto que describa la imagen')
+            .col-md-8
+              p Es uno de los frameworks más recientes para el desarrollo de apps móviles híbridas, permite la creación de apps sólo JavaScript. Utiliza el mismo diseño que React, lo que le permite componer una interfaz de usuario móvil enriquecida. Con este framework, no se crea una aplicación web móvil, tampoco una aplicación basada en HTML5 o una aplicación híbrida típica. Es posible crear una aplicación móvil muy similar a una nativa, casi idéntica a una aplicación desarrollada con Objective-C o Java. React Native utiliza los mismos bloques de creación de interfaz de usuario nativa que las aplicaciones iOS y Android. 
 
 
+          .row.justify-content-center.align-items-center(titulo="Ionic")       
+            .col-md-2
+              figure
+                img(src='@/assets/curso/t4-ic2.svg', alt='Texto que describa la imagen')
+            .col-md-8
+              p Es un SDK de código abierto para el desarrollo de aplicaciones móviles híbridas mediante tecnologías web como CSS, HTML5 y Sass. Las aplicaciones pueden distribuirse a través de tiendas de aplicaciones nativas para instalarse en dispositivos aprovechando Cordova.
+
+          .row.justify-content-center.align-items-center(titulo="NativeScript")       
+            .col-md-2
+              figure
+                img(src='@/assets/curso/t4-ic3.svg', alt='Texto que describa la imagen')
+            .col-md-8
+              p Las aplicaciones nativescript se crean con JavaScript o mediante cualquier lenguaje que utilice JavaScript, como TypeScript.  Tiene buena integración con las versiones Angular más recientes, incluidas las características de full-stack, la integración con la CLI Angular, la compatibilidad con enrutadores y la generación de código. También incluye la integración con Vue, que permite el uso de la VUE CLI, Vuex, y otras características útiles de Vue.js. Las aplicaciones móviles desarrolladas con nativescript son aplicaciones totalmente nativas y usan las mismas API desarrolladas para Xcode o Android Studio. Esto significa que obtiene la interfaz de usuario nativa de la plataforma sin WebViews y se ejecuta con el rendimiento nativo.
+
+          .row.justify-content-center.align-items-center(titulo="Quasar")       
+            .col-md-2
+              figure
+                img(src='@/assets/curso/t4-ic4.svg', alt='Texto que describa la imagen')
+            .col-md-8
+              p Está soportado y sustentado por Vue.js y permite a los desarrolladores escribir código una sola vez e implementar simultáneamente, como un sitio web (SPA, PWA, SSR), aplicación móvil iOS, Android  y escritorio, usando Electron, con una sola base de código. Utiliza HTML/CSS/JS, optimiza el uso de caché, permite optimizar el código utilizando Tree shaking. Es 100% libre, de código abierto y tiene licencia bajo el MIT, tiene buena documentación y una comunidad activa trabajando en mejorar su funcionalidad.
+
+          .row.justify-content-center.align-items-center(titulo="Kendo UI")       
+            .col-md-2
+              figure
+                img(src='@/assets/curso/t4-ic5.svg', alt='Texto que describa la imagen')
+            .col-md-8
+              p Proporciona una colección extensa de componentes de interfaz de usuario de JavaScript, con bibliotecas para jQuery, Angular, React y Vue para crear aplicaciones móviles híbridas. Cuenta con una buena base de widgets que se pueden usar con jQuery, Angular, React y Vue. Está dirigido a clientes empresariales, no cuenta con versión gratuita disponible por ahora. 
+
+          .row.justify-content-center.align-items-center(titulo="Framework7")       
+            .col-md-2
+              figure
+                img(src='@/assets/curso/t4-ic6.svg', alt='Texto que describa la imagen')
+            .col-md-8
+              p Framework HTML móvil gratuito y de código abierto, permite desarrollar apps móviles híbridas, web y web progresivas (PWAs) con aspecto y sensación nativas. Se puede integrar con herramientas adicionales como Electron y NW.js, lo que le permite crear aplicaciones de escritorio nativas.  Se utiliza como herramienta para desarrollo de prototipos para generar el diseño de aplicaciones. Se centra en el diseño de materiales para iOS y Google, ofreciendo mejor experiencia y simplicidad.
+
+          .row.justify-content-center.align-items-center(titulo="Aurelia")       
+            .col-md-2
+              figure
+                img(src='@/assets/curso/t4-ic7.svg', alt='Texto que describa la imagen')
+            .col-md-8
+              p Se podría definir como una colección de módulos JavaScript modernos, que cuando se utilizan juntos, conforman una plataforma de desarrollo ideal para crear aplicaciones web, programas de escritorio y aplicaciones móviles. La particularidad que presenta este framework es que cualquiera de los módulos de Aurelia se puede utilizar de manera independiente en cualquier proyecto de JavaScript o Node.js.  Está enfocado para desarrolladores que prefieren utilizar JavaScript o TypeScript. 
+          
+          .row.justify-content-center.align-items-center(titulo="Ext JS")       
+            .col-md-2
+              figure
+                img(src='@/assets/curso/t4-ic8.svg', alt='Texto que describa la imagen')
+            .col-md-8
+              p Enfocado en el desarrollo de soluciones empresariales para crear aplicaciones web móviles multiplataforma con HTML5 y JavaScript que consumen muchos datos. Se define como uno de los mejores frameworks JavaScript.
+
+          
+          .row.justify-content-center.align-items-center(titulo="Xamarin")       
+            .col-md-2
+              figure
+                img(src='@/assets/curso/t4-ic9.svg', alt='Texto que describa la imagen')
+            .col-md-8
+              p Tiene implementaciones multiplataforma de la infraestructura de lenguaje común (CLI) y las especificaciones de lenguaje Microsoft .NET.  Tiene su base de código compartida con C#, los desarrolladores pueden usar herramientas de Xamarin para escribir aplicaciones nativas de Android, iOS y Windows, con interfaces de usuario nativas y compartir código en varias plataformas.
+
+          .row.justify-content-center.align-items-center(titulo="Unity")       
+            .col-md-2
+              figure
+                img(src='@/assets/curso/t4-ic10.svg', alt='Texto que describa la imagen')
+            .col-md-8
+              p Es uno de los motores de videojuegos más reconocidos. Se puede utilizar en los tres entornos de desarrollo más populares: Linux, Windows, OS X. Y tiene la posibilidad de compilar para gran número de plataformas, entre ellas Android, iOS, Windows Phone. La facilidad que ofrece este motor gráfico permite el desarrollo de videojuegos 2D y 3D, cuenta con unas herramientas de optimización de desarrollo y desempeño que incluso permiten añadir compras, publicidad y realizar analíticas de una manera sencilla y completamente integrada, Posee una de las comunidades de desarrolladores más activa, tiene disponible su propia tienda.
 
 
 
--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -7,20 +7,405 @@
         span  5
       h1 Etapas en el desarrollo de apps    
 
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/t5-i1.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-4.shadow-box
+            p.mb-0 Para desarrollar una aplicación móvil, se deben seguir una serie de etapas, similares a las que se realizan cuando se desarrolla cualquier tipo de software, pero aplicadas a una app móvil. Es fundamental conocerlas para desarrollar un producto de calidad. El desarrollo de aplicaciones móviles no se limita solo a la generación del código fuente o la interfaz de usuario, está compuesto por otras etapas que se ilustran a continuación en la siguiente figura:
+
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        figcaption.customCaption.mb-2  <b>Figura</b> Etapas del desarrollo de apps
+        figure 
+          img.nW(src='@/assets/curso/t5-it2.svg', alt='tipos de contenidos móviles')
+
+    #t_5_1.titulo-segundo
+      h2 Conceptualización
+    p.mb-3  Concebir la idea de la aplicación. Por lo general, esta etapa se fundamenta en la identificación de las necesidades y/o problemas de los usuarios que se pretende resolver a través de la app. Se debe realizar una investigación preliminar y un levantamiento de información que debe permitir la comprobación y viabilidad del concepto o idea.  
+    p.mb-5  La conceptualización define de manera general la funcionalidad de la aplicación. La siguiente figura representa este concepto.
+
+    .row.justify-content-center.mb-5
+      .col-lg-8
+        figcaption.customCaption.mb-2  <b>Figura</b> Etapas del desarrollo de apps
+        figure 
+          img.nW(src='@/assets/curso/t5-p1.png', alt='tipos de contenidos móviles')
+        figcaption  <b>Nota.</b> Tomada de CuponU (2021).
+
+    p.mb-5  La conceptualización incluye elementos como los especificados en la siguiente tabla:
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        figcaption.customCaption.mb-2  <b>Tabla</b> Definición de aplicación
+        .tabla-a.color-acento-botones 
+          table
+            thead
+              tr
+                th Ideación
+                th Investigación
+                th Formalización de la idea
+            tbody
+              tr
+                td
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | Definir la idea.
+                    li 
+                      i.fas.fa-check
+                      | Inventar, pensar una solución nueva para un problema detectado. Mejorar un proceso ya existente.
+                td
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | Investigar si ya existe en el mercado.
+                    li 
+                      i.fas.fa-check
+                      | Generar un valor agregado.
+                    li 
+                      i.fas.fa-check
+                      | Inventar, pensar algo nuevo.
+                    li 
+                      i.fas.fa-check
+                      | Identificar las necesidades y problemas de los usuarios.                                                           
+                td La idea corresponde a la necesidad detectada en la investigación preliminar y se comprueba la viabilidad de desarrollar la aplicación móvil.
+
+
+    Separador
+    br
+    br
+    #t_5_2.titulo-segundo
+      h2 Definición
+    p.mb-5  Se identifica y define el grupo de usuarios objetivo que serán los que utilizarán la aplicación trabajando sobre sus características generales.  Con el resultado de esta caracterización, se establece la funcionalidad y alcance del proyecto, especificando el diseño y la interfaz gráfica a trabajar.  En la siguiente tabla se definen algunos de los aspectos incluidos en esta etapa.
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        figcaption.customCaption.mb-2  <b>Tabla</b> Definición de aplicación
+        .tabla-a.color-acento-botones.mb-5 
+          table
+            thead
+              tr
+                th Alcance de la app
+                th Definición de usuarios
+                th Definición funcional
+            tbody
+              tr
+                td Se determina el alcance del proyecto, diseño y desarrollo de la app.
+                td Se describen detalladamente las características de los usuarios para quienes se diseñará la aplicación.
+                td Usando la metodología viaje del usuario, se identifican las características y se establece la funcionalidad óptima de la aplicación. 
+
+    
+    p.mb-5  En la siguiente figura se muestran los elementos relacionados con la metodología del viaje del usuario, los cuales describen de manera general el comportamiento de un usuario frente al uso de una app.
+    .row.justify-content-center.mb-5
+      .col-lg-8
+        figcaption.customCaption.mb-2  <b>Figura</b> Metodología viaje del usuario
+        figure 
+          img.nW(src='@/assets/curso/t5-t2.svg', alt='tipos de contenidos móviles')
+
+
+    Separador
+    br
+    br
+    #t_5_3.titulo-segundo
+      h2 El viaje de usuario
+    .row.mb-5
+      .col-lg-7
+        p.mb-3  Permite realizar una previsualización de cómo se desarrollará la aplicación, generando la secuencia de pasos que representan el escenario en el que un usuario puede interactuar con el proyecto. Sirve para entender la dinámica de la aplicación y permite diseñar una mejor experiencia de usuario.
+        p.mb-3  Permite comunicar de manera clara la idea del proyecto a todas las partes interesadas, simulando el comportamiento del usuario: cómo va a interactuar con el proyecto, detectando errores tempranos antes de iniciar el diseño.
+        p.mb-3  Ayuda a definir la interfaz y el flujo de trabajo sobre los diferentes escenarios, clarificando las tareas que el usuario va realizar y qué tipo de interfaz de usuario es óptima para llevar a cabo las operaciones.
+        .cajon.color-acento-contenido.p-4
+          p Por lo general, se utiliza al inicio del proyecto para visualizar los requisitos y funcionalidad, ayuda a sustentar otras actividades importantes en el diseño, también se utiliza al finalizar el desarrollo para verificar la funcionalidad, desempeño y nivel de satisfacción del cliente.
+      .col-lg-5
+        figure 
+          img(src='@/assets/curso/t5-i2.jpg', alt='Dos personas charlando frente a un computador')
+
+    p.mb-5  Los viajes de usuario representan el recorrido que van a realizar los clientes con el producto, por lo que se debe pensar primero en cómo son y cómo se comportan, con el fin de crear un viaje de usuario eficaz.
+    p.mb-5  La siguiente tabla incluye las preguntas a responder con un viaje de usuario.
+
+    .row.justify-content-center.mb-5
+      .col-lg-12
+        figcaption.customCaption.mb-2  <b>Tabla</b> Preguntas para resolver en un viaje de usuario
+        .tabla-a.color-acento-botones.mb-5 
+          table
+            thead
+              tr
+                th Contexto
+                th Progresión
+                th Dispositivos
+                th Funcionalidad
+                th Emoción
+            tbody
+              tr
+                td 
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | ¿Dónde está el usuario?
+                    li 
+                      i.fas.fa-check
+                      | ¿Qué hay a su alrededor?
+                    li 
+                      i.fas.fa-check
+                      | ¿Existen factores externos que puedan distraerlo?
+                    li 
+                      i.fas.fa-check
+                      | ¿Cuál es su motivación?  
+                              
+                td
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | ¿De qué manera cada paso le permite llegar al siguiente?
+                    li 
+                      i.fas.fa-check
+                      | ¿La actividad se hace más sencilla con el uso de la aplicación?                  
+                td 
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | ¿Qué dispositivo está utilizando?
+                    li 
+                      i.fas.fa-check
+                      | ¿Qué características tiene el dispositivo?  
+
+                td 
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | ¿Qué tipo de funcionalidades está esperando?
+                    li 
+                      i.fas.fa-check
+                      | ¿Solucionó el problema?
+                td 
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | ¿Cuál es su estado emocional en cada paso?
+                    li 
+                      i.fas.fa-check
+                      | ¿Está entretenido, aburrido, enfadado?   
+                    li 
+                      i.fas.fa-check
+                      | ¿Cumplió con sus expectativas?           
+
+    .row.mb-5.justify-content-center
+      .col-lg-2
+        figure 
+          img.nW(src='@/assets/curso/t5-ic1.svg', alt='imagen decorativa')
+      .col-lg-6
+        ul.lista-ul--color
+          li 
+            i.fas.fa-check
+            | Una imagen de la persona que va a hacer ese viaje.
+          li 
+            i.fas.fa-check
+            | Un título que resuma el viaje.
+          li 
+            i.fas.fa-check
+            | Un resumen en texto o en imágenes de cada paso.
+          li 
+            i.fas.fa-check
+            | Un ejemplo de lo que está sucediendo en cada paso (si se considera necesario).
+          li 
+            i.fas.fa-check
+            | El dispositivo utilizado.
+          li 
+            i.fas.fa-check
+            | Los cambios en el trayecto actual (toma de decisiones).
+          li 
+            i.fas.fa-check
+            | Beneficios para el usuario y/o negocio.
+          li 
+            i.fas.fa-check
+            | Identificar dificultades o cuellos de botella.
+
+            
+             
+
+    Separador
+    br
+    br
+    #t_5_4.titulo-segundo
+      h2 Diseño
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/t5-i3.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-4.shadow-box
+            p.mb-0 Es el aspecto visual de la aplicación, se genera utilizando wireframes o mockups, se crea un prototipo, permitiendo de esta manera que el cliente y/o los usuarios puedan visualizar y probar la estructura gráfica y funcional de la aplicación. Es el momento oportuno para realizar cambios o modificaciones recomendadas por el cliente o los usuarios sin afectar el proceso de desarrollo.  La siguiente tabla resume los elementos incluidos en el proceso de diseño.
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        figcaption.customCaption.mb-2  <b>Tabla</b> Fases del diseño de una aplicación
+        .tabla-a.color-acento-botones
+          table
+            thead
+              tr
+                th Wireframes
+                th Prototipos 
+                th Test con usuarios
+                th Diseño visual
+            tbody
+              tr
+                td 
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | Define el contenido y la posición de los elementos de la app.
+                    li 
+                      i.fas.fa-check
+                      | Representa cómo será la interacción de los elementos y los módulos.    
+                td Se definen los prototipos para ser probados con usuarios.
+                td 
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | Análisis funcionales.
+                    li 
+                      i.fas.fa-check
+                      | Pruebas de movilidad.
+                    li 
+                      i.fas.fa-check
+                      | Pruebas de interfaz.
+                    li 
+                      i.fas.fa-check
+                      | Pruebas de accesibilidad.
+                    li 
+                      i.fas.fa-check
+                      | Pruebas de usabilidad del prototipo.  
+                td 
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | Diseño visual acabado que será provisto al desarrollador.
+                    li 
+                      i.fas.fa-check
+                      | Pantallas modelo, para la programación del código.
+
+
+    Separador
+    br
+    br
+    #t_5_5.titulo-segundo
+      h2 Desarrollo
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/t5-i4.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-4.shadow-box
+            p.mb-0 Corresponde al inicio de todo el proceso de construcción y desarrollo de la app en el lenguaje de desarrollo y plataforma definidos en las etapas previas.  La siguiente tabla ilustra las fases involucradas en el desarrollo de la aplicación.
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        figcaption.customCaption.mb-2  <b>Tabla</b> Fases del desarrollo de una aplicación
+        .tabla-a.color-acento-botones
+          table
+            thead
+              tr
+                th Programación
+                th Base de datos
+                th Integración de API
+                th Corrección de errores en código
+            tbody
+              tr
+                td 
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | Lenguaje de programación y framework.
+                    li 
+                      i.fas.fa-check
+                      | El desarrollador crea la estructura sobre la cual construirá el funcionamiento de la aplicación.       
+                td Integración de la aplicación a la base de datos.
+                td Integración de las API a la aplicación.
+                td Corrección de errores funcionales para asegurar el correcto funcionamiento y desempeño de la app.
 
 
 
 
+    Separador
+    br
+    br
+    #t_5_6.titulo-segundo
+      h2 Pruebas
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/t5-i5.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-4.shadow-box
+            p.mb-0 Para garantizar el correcto funcionamiento de la app, es importante realizar una serie de pruebas; a través de ellas, se puede medir la aceptación y usabilidad de la aplicación. Estas fases se describen en la siguiente tabla.
+
+    .row.justify-content-center.mb-5
+      .col-lg-12
+        figcaption.customCaption.mb-2  <b>Tabla</b> Fases de prueba de una aplicación
+        .tabla-a.color-acento-botones
+          table
+            thead
+              tr
+                th Prueba de usabilidad
+                th Pruebas de compatibilidad
+                th Pruebas de seguridad
+                th Pruebas de recuperabilidad
+                th Pruebas de regresión
+            tbody
+              tr
+                td Identificar problemas en la experiencia del usuario para poder determinar su nivel de satisfacción.
+                td 
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | Rendimiento de la app en función a los diferentes dispositivos.
+                    li 
+                      i.fas.fa-check
+                      | Evaluar aspectos como el sistema operativo, resolución de la pantalla, memoria.               
+                td
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-check
+                      | Evitar el acceso a datos personales.
+                    li 
+                      i.fas.fa-check
+                      | Analizar y evaluar la seguridad para evitar amenazas de virus o hackers.                  
+                td Efectividad de recuperación de posible error o fallo (software, red, hardware).
+                td Modificar el código para eliminar errores encontrados en las etapas anteriores.
+
+    Separador
+    br
+    br
+
+    #t_5_7.titulo-segundo
+      h2 Publicación
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/t5-i6.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-4.shadow-box
+            p.mb-0 Al llegar a esta fase, se asume que la aplicación se encuentra lista para publicación. Se definen los requerimientos de publicación, realizando la configuración según las políticas de la tienda de aplicaciones.  En la siguiente tabla se describen sus fases.
+
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        figcaption.customCaption.mb-2  <b>Tabla</b> Fases en la publicación de una aplicación
+        .tabla-a.color-acento-botones.mb-5 
+          table
+            thead
+              tr
+                th Lanzamiento
+                th Seguimiento
+                th Actualización
+            tbody
+              tr
+                td La aplicación es puesta a disposición de los usuarios en las tiendas digitales.
+                td Seguimiento a través de analíticas, estadísticas y comentarios de usuarios.
+                td Se mide el desempeño de la app, se corrigen errores, se realizan mejoras y se actualizan futuras versiones.
 
 
-
-
-
-
-
-
-
-
+    
 
 
 </template>
